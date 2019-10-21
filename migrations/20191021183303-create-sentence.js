b6120a83ec6266e634df458f974ee15b6911cffc
Michael Ion <michael.ion.mi@outlook.com>
2019-10-21 16:44:09 -0400

    all seeders migrations done

--- migrations/20191021183303-create-sentence.js
+++ migrations/20191021183303-create-sentence.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      word: {
+     sentence: {
         type: Sequelize.STRING
       },
       createdAt: {
--- seeders/20191021191720-keyWord.js
+++ seeders/20191021191720-keyWord.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const words = [{
   word:"Funny",
 },
