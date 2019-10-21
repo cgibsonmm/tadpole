63aafe52371f99f2b4591453e4dff9714b25a578
corey gibson <cgibsonmm@gmail.com>
2019-10-21 14:55:57 -0400

    main nav and footer route showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Nav from './components/sharedComponents/Nav';
+import Main from './components/Main'
+import Footer from './components/sharedComponents/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <>
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Nav from './sharedComponents/Nav';
+import Footer from './sharedComponents/Footer';
+
+export default function Main() {
+  return (
+    <>
+      {/* Routes */}
+      main
+    </>
+  )
+}
