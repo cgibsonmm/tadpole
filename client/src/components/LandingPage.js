7a15915d266d13a849bf241fb409d5bc1871de28
Amy Jerkovich <amy.jerkovich@gmail.com>
2019-10-23 08:49:13 -0400

    style main

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -25,6 +25,7 @@ export default function LandingPage() {
       <h5>let us help you find your personal brand</h5>
       <h4>Enter name to start and select 3 key words</h4>
       <NameForm handleInput={handleInput} />
+      <button className="submit">Submit</button>
       <br />
       <br />
       <ButtonList />
